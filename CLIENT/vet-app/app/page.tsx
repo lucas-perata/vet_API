ee62881d8fefce0efeac6fb55b453f0c418e47b0
lucas-perata <contacto.perata@gmail.com>
2024-05-27 12:22:46 -0300

    basic filter owner/vets

--- CLIENT/vet-app/app/page.tsx
+++ CLIENT/vet-app/app/page.tsx
@@ -1,17 +1,7 @@
-"use client"
-import useStore from "@/store/store";
-import { getRole } from "./actions/getRole";
-import { VetMain } from "./dashboard/vetMain";
-import { OwnerMain } from "./dashboard/ownerMain";
-
+import React from "react";
 export default function Home() {
-  const token = useStore((state) => state.token());
-  const role = getRole(token);
+
   return (
-    <div>
-      {
-        role == "Vet" ? <VetMain /> : <OwnerMain />
-      }
-    </div>
+    <div>Landing page</div>
   );
 }
